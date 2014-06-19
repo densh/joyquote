ad4f519cc2ded59d67c10acc2ceaa46b55236b76
Denys Shabalin <denys.shabalin@typesafe.com>
2014-06-19 16:56:39 +0200

    Step 4: moving parsing to compile-time
    We use macros to remove all of the runtime parsing overhead. Apart from
    moving code to the macro implementation we also need to implement
    Joy tree to Scala tree conversion (called lifting). This is needed
    because macro has to return a tree that represents a value rather
    than a value itself.
    
    It's important to highlight that we opt in to use whitebox macros rather
    than blackbox ones as it will give as ability to refine return type of
    the quotation to specific subtypes of Joy tree.
    
    For example in a few steps we'll make following work:
    
        scala> j"42"
        res0: joy.Joy.Int = 42
    
    Here res0 is Joy.Int rather than just Joy thanks to whiteboxity.

--- src/main/scala/JoyQuote.scala
+++ src/main/scala/JoyQuote.scala
@@ -1,5 +1,26 @@
+import scala.language.experimental.macros
+import scala.reflect.macros.whitebox.Context
+
 package object joy {
   implicit class JoyQuote(ctx: StringContext) {
-    def j(args: Joy*): Joy = Joy.parse(ctx.parts.head).get
+    def j(args: Joy*): Joy = macro JoyQuoteImpl.apply
+  }
+}
+
+package joy {
+  private[joy] class JoyQuoteImpl(val c: Context) {
+    import c.universe._
+
+    lazy val q"$_($_(..${parts: List[String]})).j(..$args)" = c.macroApplication
+
+    implicit def lift[J <: Joy]: Liftable[J] = Liftable {
+      case Joy.Int(value)    => q"_root_.joy.Joy.Int($value)"
+      case Joy.Bool(value)   => q"_root_.joy.Joy.Bool($value)"
+      case Joy.Name(value)   => q"_root_.joy.Joy.Name($value)"
+      case Joy.Quoted(joys)  => q"_root_.joy.Joy.Quoted($joys)"
+      case Joy.Program(joys) => q"_root_.joy.Joy.Program($joys)"
+    }
+
+    def apply(args: Tree*) = lift(Joy.parse(parts.head).get)
   }
 }
